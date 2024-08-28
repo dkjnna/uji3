4124a2aa06ff775f6ef576ba143b37a4a211af2c
dkjnna <faridfadilah70@gmail.com>
2024-08-28 22:37:00 +0700

    Menambahkan halaman home

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav class="nav">
+    <div class="menu-icon"></div>
+    <img src="asset/img/logo.png" alt="" class="logo">
+    <ul class="ul-menu">
+        <li><a href="\">Home</a></li>
+        <li><a href="\">About</a></li>
+        <li><a href="\">Ticket</a></li>
+        <li><a href="\">Layout</a></li>
+        <li><a href="\">Gallery</a></li>
+    </ul>
+    <img src="asset/img/user" alt="" class="user">
+</nav>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="asset/css/main.css">
+</head>
+<body>
+    <div class="container">
+        <!-- Navbar -->
+        <?php require ('includes/navbar.php')?>
+
+        <!-- Home -->
+        <div class="home" id="home">
+            <img class="bg-home" src="asset/img/bg.jfif" alt="">
+            <div class="foto-home">
+                <img src="asset/img/bruno.png" alt="" class="foto-bruno">
+            </div>
+            <div class="content-home">
+                <h2 class="subjudul">Comming Soon</h2>
+                <h1 class="judul">Bruno On Stage</h1>
+                <h3 class="location">Jakarta International Stadium</h3>
+                <div class="informasi">
+                    <p>Tour for the Latest Album - '24K Magic'.</p>
+                    <p>Sunday, September 15, 2024</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Abot -->
+
+
+        <!-- TIcket -->
+
+
+        <!-- Layout -->
+
+
+        <!-- Gellery -->
+
+
+        <!-- Footer -->
+
+        
+    </div>
+</body>
+</html>
